4470bc23802ed1f13e70de23e63ab475fdc3f927
rafael <rafaelfpinho@usp.br>
2022-09-07 12:53:46 -0300

    Updated strings and added theme colors

---
+++ lib/ui/config/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Theme's Colors
+  static const Color accentColor = Color(0xFF31FF00);
+  static Color accentColorDisabled = accentColor.withOpacity(0.38);
+  static const Color secondaryColor = Color(0xFF005BA5);
+
+
+  //General Colors
+  static const Color shimmerBackground = Color(0xFFe1e1e1);
+  static const Color shimmerHighlight = Colors.white;
+  static const Color buttonGray = Color(0xFFDBDBDB);
+}
--- lib/ui/config/app_theme.dart
+++ lib/ui/config/app_theme.dart
@@ -4,4 +4,7 @@ ThemeData lightTheme = ThemeData(
   primaryColor: Color(0xFF4ED250),
   primarySwatch: Colors.green,
   backgroundColor: Color(0xFF4ED250),
+  scaffoldBackgroundColor: Colors.white,
+
+  // buttonColor: Color(0xFF4ED250)
 );
